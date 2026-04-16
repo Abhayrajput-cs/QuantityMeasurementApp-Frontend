bb3196bc83582b11e4738ff5c915b3cdd4f67222
Abhay Pratap singh <122587173+Abhayrajput-cs@users.noreply.github.com>
2026-04-16 16:56:06 +0530

    Remove trailing slash from API_BASE_URL

--- angular-app/src/app/app.constants.ts
+++ angular-app/src/app/app.constants.ts
@@ -1,3 +1,3 @@
-export const API_BASE_URL = 'http://35.172.201.210:8080/';
+export const API_BASE_URL = 'http://35.172.201.210:8080';
 export const AUTH_TOKEN_STORAGE_KEY = 'qm-auth-token';
 export const THEME_STORAGE_KEY = 'qm-theme';
