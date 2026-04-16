bf958ea4e1944955582b23beda2c31611e36a820
Abhay Pratap singh <122587173+Abhayrajput-cs@users.noreply.github.com>
2026-04-16 15:54:54 +0530

    Update API URL to use API_BASE_URL constant
    Abhay Upadred

--- angular-app/src/app/services/quantity-api.service.ts
+++ angular-app/src/app/services/quantity-api.service.ts
@@ -232,7 +232,7 @@ export class QuantityApiService {
 
     return firstValueFrom(
       this.http.get<any[]>(
-        `http://localhost:8080/api/v1/quantities/all`,
+        `${API_BASE_URL}/api/v1/quantities/all`,
         {
           params,
           headers: this.getHeaders()   // ✅ CLEAN
